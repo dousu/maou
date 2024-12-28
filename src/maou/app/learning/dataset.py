d83f838fcef6e5a53944c695a1c8440c11af2e18
Hiroki Sudo <34547252+dousu@users.noreply.github.com>
2024-12-28 08:10:39 +0000

    fix(kifdataset): 最初にtransformするとメモリ足りなくて落ちるので後からtransformに戻す

--- src/maou/app/learning/dataset.py
+++ src/maou/app/learning/dataset.py
@@ -19,37 +19,60 @@ class KifDataset(Dataset):
             tuple[torch.Tensor, tuple[torch.Tensor, torch.Tensor]],
         ],
     ):
+        # 最初にtransformしないパターン
+        self.hcps: list[np.ndarray]
+        self.transform = transform
         # 各局面を棋譜の区別なくフラットにいれておく
-        hcpes = np.concatenate(
+        self.hcpes = np.concatenate(
             [np.fromfile(path, dtype=HuffmanCodedPosAndEval) for path in paths]
         )
-        self.transformed_data: list[
-            tuple[torch.Tensor, tuple[torch.Tensor, torch.Tensor]]
-        ] = []
-        for i, path in enumerate(paths):
-            if i % 100000 == 0:
-                self.logger.info(f"進捗: {i / len(paths) * 100}%")
-            try:
-                hcpes = np.fromfile(path, dtype=HuffmanCodedPosAndEval)
-                self.transformed_data.extend(
-                    [
-                        transform(
-                            hcpe["hcp"],
-                            hcpe["bestMove16"],
-                            hcpe["gameResult"],
-                            hcpe["eval"],
-                        )
-                        for hcpe in hcpes
-                    ]
-                )
-            except Exception:
-                self.logger.error(f"path: {path}")
-        self.logger.info(f"{len(self.transformed_data)} samples")
+        self.logger.info(f"{len(self.hcpes)} samples")
+
+        # 最初にtransformするパターン
+        # # 各局面を棋譜の区別なくフラットにいれておく
+        # hcpes = np.concatenate(
+        #     [np.fromfile(path, dtype=HuffmanCodedPosAndEval) for path in paths]
+        # )
+        # self.transformed_data: list[
+        #     tuple[torch.Tensor, tuple[torch.Tensor, torch.Tensor]]
+        # ] = []
+        # for i, path in enumerate(paths):
+        #     if i % 100000 == 0:
+        #         self.logger.info(f"進捗: {i / len(paths) * 100}%")
+        #     try:
+        #         hcpes = np.fromfile(path, dtype=HuffmanCodedPosAndEval)
+        #         self.transformed_data.extend(
+        #             [
+        #                 transform(
+        #                     hcpe["hcp"],
+        #                     hcpe["bestMove16"],
+        #                     hcpe["gameResult"],
+        #                     hcpe["eval"],
+        #                 )
+        #                 for hcpe in hcpes
+        #             ]
+        #         )
+        #     except Exception:
+        #         self.logger.error(f"path: {path}")
+        # self.logger.info(f"{len(self.transformed_data)} samples")
 
     def __len__(self) -> int:
-        return len(self.transformed_data)
+        # 最初にtransformしないパターン
+        return len(self.hcpes)
+
+        # 最初にtransformするパターン
+        # return len(self.transformed_data)
 
     def __getitem__(
         self, idx: int
     ) -> tuple[torch.Tensor, tuple[torch.Tensor, torch.Tensor]]:
-        return self.transformed_data[idx]
+        # 最初にtransformしないパターン
+        return self.transform(
+            self.hcpes[idx]["hcp"],
+            self.hcpes[idx]["bestMove16"],
+            self.hcpes[idx]["gameResult"],
+            self.hcpes[idx]["eval"],
+        )
+
+        # 最初にtransformするパターン
+        # return self.transformed_data[idx]
